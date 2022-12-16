58b143ae478ca13a65fe93d1b3d2041a3bf9b89b
VovaWeepy <117993619+VovaWeepy@users.noreply.github.com>
2022-12-16 03:28:32 +0300

    Add files via upload

---
+++ КДР-2.1.pas
@@ -0,0 +1,12 @@
+﻿var
+ s, s1: string;
+ p:boolean;
+begin
+ readln(s);
+ readln(s1);
+ for var i:=1 to length(s)-length(s1) do
+ if s[i:i+length(s1)]=s1 then
+   p:=true;
+if p then write('является') 
+else write ('не является')
+end.
---
+++ КДР-2.pas
@@ -0,0 +1,31 @@
+﻿uses crt;
+var a:array[1..100]of integer;
+    n,i,j:integer;
+    
+begin
+clrscr;
+randomize;
+write('n=');readln(n);
+writeln('Massiv:');
+for i:=1 to n do
+  begin
+    a[i]:=random(5);
+    write(a[i],' ');
+  end;
+writeln;
+i:=1;
+while i<=n do
+   begin
+    if a[i]=0 then
+       begin
+         for j:=i to n-1 do
+         a[j]:=a[j+1];
+         n:=n-1;
+       end
+    else i:=i+1;
+   end;
+writeln('Rezultat:');
+for i:=1 to n do
+write(a[i],' ');
+readln
+end.
